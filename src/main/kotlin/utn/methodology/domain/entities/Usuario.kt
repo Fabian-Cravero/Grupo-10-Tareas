4329ec28c27ee08fe20ee8c6d50b450b5cb3e754
Fabian-Cravero <fabiancravero92@gmail.com>
2024-08-30 21:49:27 -0300

    actualizaci4

--- src/main/kotlin/utn/methodology/domain/entities/Usuario.kt
+++ src/main/kotlin/utn/methodology/domain/entities/Usuario.kt
@@ -13,5 +13,30 @@ class Usuario (
     var username: String,
     var email: String,
     var password: String,
-)
+){
+    companion object {
+        fun fromPrimitives(primitives: Map<String, String>): Usuario {
+
+            val user = Usuario(
+                primitives["uuid"] as String,
+                primitives["name"] as String,
+                primitives["username"] as String,
+                primitives["email"] as String,
+                primitives["password"] as String,
+            );
+
+            return user;
+        }
+    }
+    fun toPrimitives(): Map<String, String> {
+        return mapOf(
+            "uuid" to this.uuid,
+            "name" to this.name,
+            "username" to this.username,
+            "email" to this.email,
+            "password" to this.password,
+        )
+    }
+
+}
 
