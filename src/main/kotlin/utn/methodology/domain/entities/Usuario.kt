316f0f865a0d004bec7a680d000067e6835702e3
Santi-Peralta <peraltasantiago540@gmail.com>
2024-09-04 12:31:57 -0300

    Cambios guardados hasta historia 3

--- src/main/kotlin/utn/methodology/domain/entities/Usuario.kt
+++ src/main/kotlin/utn/methodology/domain/entities/Usuario.kt
@@ -2,11 +2,6 @@ package utn.methodology.domain.entities
 
 import java.util.UUID
 
-//Historia 1: Crear el modelo básico de usuario
-//Crear un modelo de usuario con los atributos uuid, nombre, username, email, y contraseña.
-//El uuid deberá ser de tipo autoincremental y único.
-//El modelo deberá tener carácter de Entidad, se utilizará para realizar consultas a la base de datos.
-
 class Usuario (
     val uuid: String = UUID.randomUUID().toString(),
     var name: String,
---
+++ src/main/kotlin/utn/methodology/infrastructure/http/router/CreateUserRouter.kt
@@ -0,0 +1,25 @@
+package utn.methodology.infrastructure.http.router
+
+import io.ktor.http.*
+import utn.methodology.infrastructure.persistence.connectToMongoDB
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import utn.methodology.application.commands.CreateUserCommand
+import utn.methodology.infrastructure.http.actions.CreateUserAction
+
+fun Application.createUserRoutes(){
+    val mongoDatabase = connectToMongoDB()
+    routing {
+        post ("/users") {
+            println("Received POST request to /create")
+
+            val body = call.receive<CreateUserCommand>()
+
+            CreateUserAction.execute(body)
+
+            call.respond(HttpStatusCode.Created, mapOf("message" to "ok"))
+
+        }
+    }
+}
