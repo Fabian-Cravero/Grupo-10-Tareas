2b397aa0d1e7862168a3f73c3e621ba1adfd4951
JustinMagliano <jusmaglian@gmail.com>
2024-09-06 20:44:45 -0300

    historia_6

---
+++ src/main/kotlin/utn/methodology/infrastructure/http/actions/GetUserAction.kt
@@ -0,0 +1,16 @@
+package utn.methodology.infrastructure.http.actions
+
+import utn.methodology.application.commandhandlers.GetUserHandler
+import utn.methodology.application.commands.GetUserCommand
+
+class GetUserAction(private val handler: GetUserHandler) {
+
+    fun execute(query: GetUserCommand): Map<String, String> {
+        query
+            .validate()
+            .let {
+                return handler.handle(it)
+            }
+
+    }
+}
---
+++ src/main/kotlin/utn/methodology/infrastructure/http/router/GetUserRouter.kt
@@ -0,0 +1,24 @@
+package utn.methodology.infrastructure.http.router
+
+import io.ktor.http.*
+import utn.methodology.infrastructure.persistence.connectToMongoDB
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import utn.methodology.infrastructure.http.actions.GetUserAction
+import utn.methodology.infrastructure.persistence.MongoUserRepository
+
+fun Application.GetUserRouter(){
+    val mongoDatabase = connectToMongoDB()
+    val userMongoUserRepository = MongoUserRepository(mongoDatabase)
+    routing {
+
+        post ("/users") {
+
+            val users = userMongoUserRepository.findAll()
+
+            call.respond(HttpStatusCode.OK, users.map{it.toPrimitive()})
+
+        }
+    }
+}
