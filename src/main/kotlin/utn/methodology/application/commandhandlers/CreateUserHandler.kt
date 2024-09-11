30e7a2fd93e09c80f1be24dae6e4f9c1fadaa63a
Santi-Peralta <peraltasantiago540@gmail.com>
2024-09-10 21:53:25 -0300

    Cambios generados

--- src/main/kotlin/utn/methodology/application/commandhandlers/CreateUserHandler.kt
+++ src/main/kotlin/utn/methodology/application/commandhandlers/CreateUserHandler.kt
@@ -2,10 +2,11 @@ package utn.methodology.application.commandhandlers
 
 import utn.methodology.application.commands.CreateUserCommand
 import utn.methodology.domain.entities.Usuario
+import utn.methodology.infrastructure.persistence.UserMongoRepository
 import java.util.UUID
 
 class CreateUserHandler (
-    private val userRepository: MongoUserRepository
+    private val userRepository: UserMongoRepository
     ) {
         fun handle(command: CreateUserCommand) {
             val user = Usuario(
